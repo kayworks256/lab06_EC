882ec78bc7bb418e3fea5eb85db49fe83196b20f
zakruvalcaba <zakruvalcaba@gmail.com>
2021-02-24 15:35:49 -0800

    updated code to ES6

--- js/script.js
+++ js/script.js
@@ -1,25 +1,22 @@
-/*eslint-env browser*/
+// GLOBAL VARIABLES
+let futureValue;
+let investment;
+let rate;
+let years;
 
-//GLOBAL VARIABLES
-var futureValue;
-var investment;
-var rate;
-var years;
-var i;
+// COLLECT VALUES FROM THE USER
+investment = parseFloat(prompt('Enter investment amount as xxxx.xx'));
+rate = parseFloat(prompt('Enter interest rate as xx.x'));
+years = parseInt(prompt('Enter the number of years you want to invest for'));
 
-//COLLECT VALUES FROM USER
-investment = parseFloat(window.prompt("Enter investment amount as xxxx.xx"));
-rate = parseFloat(window.prompt("Enter interest rate as xx.x"));
-years = parseInt(window.prompt("Enter number of years"), 10);
-
-//CALCULATE FUTURE VALUE
+// CALCULATE FUTURE VALUE
 futureValue = investment;
-for (i = 1; i <= years; i += 1) {
+for (let i = 0; i < years; i++) {
     futureValue = futureValue + (futureValue * rate / 100);
 }
 
-//DISPLAY RESULT
-window.document.write("Investment amount: $" + investment + "<br>");
-window.document.write("Interest rate: " + rate + "%<br>");
-window.document.write("Years: " + years + "<br>");
-window.document.write("Future value: $" + parseInt(futureValue, 10));
+// DISPLAY RESULT
+document.write(`Investment amount: $${investment.toFixed(2)}<br>`);
+document.write(`Interest rate: ${rate.toFixed(2)}%<br>`);
+document.write(`Years: ${years}<br>`);
+document.write(`Future value: $${futureValue.toFixed(2)}`);
